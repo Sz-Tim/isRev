410488791988faa28eff408010ccfb61e39f6884
liometopum <tim.szewczyk@gmail.com>
2015-04-17 13:50:16 -0600

    separated exploratory and publication figures

---
+++ R_scripts/figsPublish.R
@@ -0,0 +1,13 @@
+# Statistical analyses and summary statistics
+# Tim Szewczyk
+# Created 2015 April 17
+
+
+#######
+## Load libraries, functions, data
+#######
+
+library(ggplot2); theme_set(theme_bw()); library(lme4)
+library(xlsx); library(plyr); library(vegan); library(betapart)
+source("R_scripts/FuncsGen.R")
+loadAll()
