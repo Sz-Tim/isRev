ea3a6c63e1e3c8ff6afa6f4ec459849021eca280
liometopum <tim.szewczyk@gmail.com>
2015-04-19 16:16:45 -0600

    added caps to error bars, legend to fig1a

--- R_scripts/figsPublish.R
+++ R_scripts/figsPublish.R
@@ -33,15 +33,18 @@ loadAll()
   pdf("Plots/Figure1a.pdf", width=w, height=h)
     ggplot(over.df, aes(x=abs(Latsamp))) + 
       theme_is +
-      geom_point(aes(y=sp.mnRng), size=4) + 
+      geom_point(aes(y=sp.mnRng, shape="Mean"), size=4) + 
       geom_segment(aes(xend=abs(Latsamp), 
                        y=sp.mnRng+seRng, 
-                       yend=sp.mnRng-seRng), size=1) +
+                       yend=sp.mnRng-seRng), size=1,
+                   arrow=arrow(angle=90, length=unit(0.2, "cm"), ends="both")) +
       stat_smooth(aes(y=sp.mnRng), se=FALSE, method="lm", 
                   colour="black", size=1) +
-      geom_point(aes(y=sp.medRng), size=4, shape=1) +
+      geom_point(aes(y=sp.medRng, shape="Median"), size=4) +
       stat_smooth(aes(y=sp.medRng), se=FALSE, method="lm", 
                   colour="black", size=1, linetype=2) +
+      scale_shape_manual(name="", values=c("Mean"=19,
+                                           "Median"=1)) + 
       labs(x="Degrees from equator", y="Elevational range (m)")
   dev.off()
 
@@ -53,25 +56,28 @@ loadAll()
       geom_segment(aes(xend=abs(Latsamp), 
                        y=sp.mnRng.2000+sp.seRng.2000, 
                        yend=sp.mnRng.2000-sp.seRng.2000,
-                       colour="2000m"), size=1) +
+                       colour="2000m"), size=1,
+                   arrow=arrow(angle=90, length=unit(0.2, "cm"), ends="both")) +
       stat_smooth(aes(y=sp.mnRng.2000, colour="2000m"), 
                   se=FALSE, method="lm", size=1) +
       geom_point(aes(y=sp.mnRng.1800, colour="1800m"), size=4) + 
       geom_segment(aes(xend=abs(Latsamp), 
                        y=sp.mnRng.1800+sp.seRng.1800, 
                        yend=sp.mnRng.1800-sp.seRng.1800, 
-                       colour="1800m"), size=1) +
+                       colour="1800m"), size=1,
+                   arrow=arrow(angle=90, length=unit(0.2, "cm"), ends="both")) +
       stat_smooth(aes(y=sp.mnRng.1800, colour="1800m"), 
                   se=FALSE, method="lm", size=1) +
       geom_point(aes(y=sp.mnRng.1600, colour="1600m"), size=4) + 
       geom_segment(aes(xend=abs(Latsamp), 
                        y=sp.mnRng.1600+sp.seRng.1600, 
                        yend=sp.mnRng.1600-sp.seRng.1600, 
-                       colour="1600m"), size=1) +
+                       colour="1600m"), size=1,
+                   arrow=arrow(angle=90, length=unit(0.2, "cm"), ends="both")) +
       stat_smooth(aes(y=sp.mnRng.1600, colour="1600m"), 
                   se=FALSE, method="lm", size=1) +
       scale_colour_manual(name="Truncation",
-                          values=c("2000m"="black",
+                          values=c("2000m"="gray20",
                                    "1800m"="gray50",
                                    "1600m"="gray80")) +
       labs(x="Degrees from equator", y="Elevational range (m)")
